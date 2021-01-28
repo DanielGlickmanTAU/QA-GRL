c1e19eeb40365d122b0540f3db430706730cc485
Glickman <glickman1@mail.tau.ac.il>
2021-01-28 17:17:52 +0200

    class for priting and debuging

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import torch
 from transformers import DistilBertForQuestionAnswering
 
+from utils.debug import answer_question
 from utils.model_loading import get_model_and_tokenizer
 
 model, slow_tokenizer = get_model_and_tokenizer()
@@ -42,29 +42,5 @@ Q8 = "What space station supported three manned missions in 1973-1974?"
 
 questions = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8]
 
-
-def answer_quuestion(question, context):
-    tokenized_context = slow_tokenizer.encode_plus(question, context)
-    # tokenized_question = tokenizer.encode(question)
-    # input_ids = tokenized_context.ids  +tokenized_question.ids[1:]
-    input_ids = tokenized_context['input_ids']
-    # token_type_ids = [0] * len(tokenized_context.ids) + [1] * len(tokenized_question.ids[1:])
-    # attention_mask = [1] * len(input_ids)
-    attention_mask = tokenized_context['attention_mask']
-    # padding_length = 384 - len(input_ids)
-    # if padding_length > 0:
-    #     input_ids = input_ids + ([0] * padding_length)
-    #     attention_mask = attention_mask + ([0] * padding_length)
-    # token_type_ids = token_type_ids + ([0] * padding_length)
-    # token_type_ids_all.append(token_type_ids)
-    output = model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))
-    print(question)
-    answer_words = slow_tokenizer.convert_ids_to_tokens(
-        input_ids[torch.argmax(output.start_logits): torch.argmax(output.end_logits) + 1], skip_special_tokens=True)
-    answer = ' '.join(answer_words)
-    print(answer)
-    print('----')
-
-
 for question in questions:
-    answer_quuestion(question,context)
+    answer_question(question, context, model, slow_tokenizer)
---
+++ utils/debug.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def answer_question(question, context, model, tokenizer):
+    tokenized_context = tokenizer.encode_plus(question, context)
+    # tokenized_question = tokenizer.encode(question)
+    # input_ids = tokenized_context.ids  +tokenized_question.ids[1:]
+    input_ids = tokenized_context['input_ids']
+    # token_type_ids = [0] * len(tokenized_context.ids) + [1] * len(tokenized_question.ids[1:])
+    # attention_mask = [1] * len(input_ids)
+    attention_mask = tokenized_context['attention_mask']
+    # padding_length = 384 - len(input_ids)
+    # if padding_length > 0:
+    #     input_ids = input_ids + ([0] * padding_length)
+    #     attention_mask = attention_mask + ([0] * padding_length)
+    # token_type_ids = token_type_ids + ([0] * padding_length)
+    # token_type_ids_all.append(token_type_ids)
+    output = model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))
+    print(question)
+    answer_words = tokenizer.convert_ids_to_tokens(
+        input_ids[torch.argmax(output.start_logits): torch.argmax(output.end_logits) + 1], skip_special_tokens=True)
+    answer = ' '.join(answer_words)
+    print(answer)
+    print('----')
