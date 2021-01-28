07a4db804cd6f26d9a162941350f37f803ef2d35
Glickman <glickman1@mail.tau.ac.il>
2021-01-28 17:10:22 +0200

    create utility for loading tokenizer and model

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-import os
-
 import torch
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, DistilBertForQuestionAnswering, AutoConfig
+from transformers import DistilBertForQuestionAnswering
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-
-
-def get_model_and_tokenizer(model_name='distilbert-base-uncased-distilled-squad', toknizer_model_name="distilbert-base-uncased"):
-    def _get_and_save_pretrained_tokenizer(name):
-        slow_tokenizer = AutoTokenizer.from_pretrained("%s" % name , return_token_type_ids= True)
-        config = AutoConfig.from_pretrained(name)
-        if not os.path.exists("%s/" % name):
-            os.makedirs("%s/" % name)
-            slow_tokenizer.save_pretrained("%s/" % name)
-            config.save_pretrained("%s/" % name)
-
-        return slow_tokenizer
-    slow_tokenizer = _get_and_save_pretrained_tokenizer(toknizer_model_name)
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name).to(device=device)
-    return model, slow_tokenizer
-
+from utils.model_loading import get_model_and_tokenizer
 
 model, slow_tokenizer = get_model_and_tokenizer()
 
