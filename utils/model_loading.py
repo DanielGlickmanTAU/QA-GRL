e2c076e774855d6824d4b2c4f23f85eadf144c36
Glickman <glickman1@mail.tau.ac.il>
2021-01-31 14:46:01 +0200

    rename from slow_tokenizer to tokenizer, as it is not really slow

--- utils/model_loading.py
+++ utils/model_loading.py
@@ -16,15 +16,15 @@ def get_model_and_tokenizer_for_classification(model_name='distilbert-base-uncas
 
 def _get_model_and_toknizer(model_name, toknizer_model_name, autoModelClass):
     def _get_and_save_pretrained_tokenizer(name):
-        slow_tokenizer = AutoTokenizer.from_pretrained("%s" % name, return_token_type_ids=True)
+        tokenizer = AutoTokenizer.from_pretrained("%s" % name, return_token_type_ids=True)
         config = AutoConfig.from_pretrained(name)
         if not os.path.exists("%s/" % name):
             os.makedirs("%s/" % name)
-            slow_tokenizer.save_pretrained("%s/" % name)
+            tokenizer.save_pretrained("%s/" % name)
             config.save_pretrained("%s/" % name)
 
-        return slow_tokenizer
+        return tokenizer
 
-    slow_tokenizer = _get_and_save_pretrained_tokenizer(toknizer_model_name)
+    tokenizer = _get_and_save_pretrained_tokenizer(toknizer_model_name)
     model = autoModelClass.from_pretrained(model_name).to(device=device)
-    return model, slow_tokenizer
+    return model, tokenizer
