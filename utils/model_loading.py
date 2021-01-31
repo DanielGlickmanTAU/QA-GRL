ad1d562568c7267c98776e4cd7f844753d513baf
Glickman <glickman1@mail.tau.ac.il>
2021-01-31 13:53:59 +0200

    minor

--- utils/model_loading.py
+++ utils/model_loading.py
@@ -7,8 +7,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def get_model_and_tokenizer_for_qa(model_name='distilbert-base-uncased-distilled-squad', toknizer_model_name="distilbert-base-uncased"):
+    return _get_model_and_toknizer(model_name, toknizer_model_name, AutoModelForQuestionAnswering)
+
+
+def _get_model_and_toknizer(model_name, toknizer_model_name, autoModelClass):
     def _get_and_save_pretrained_tokenizer(name):
-        slow_tokenizer = AutoTokenizer.from_pretrained("%s" % name , return_token_type_ids= True)
+        slow_tokenizer = AutoTokenizer.from_pretrained("%s" % name, return_token_type_ids=True)
         config = AutoConfig.from_pretrained(name)
         if not os.path.exists("%s/" % name):
             os.makedirs("%s/" % name)
@@ -16,6 +20,7 @@ def get_model_and_tokenizer_for_qa(model_name='distilbert-base-uncased-distilled
             config.save_pretrained("%s/" % name)
 
         return slow_tokenizer
+
     slow_tokenizer = _get_and_save_pretrained_tokenizer(toknizer_model_name)
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name).to(device=device)
+    model = autoModelClass.from_pretrained(model_name).to(device=device)
     return model, slow_tokenizer
