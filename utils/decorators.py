4cc677b521fbcec4f516757024a1ce4e6d50c093
Glickman <glickman1@mail.tau.ac.il>
2021-02-02 09:21:19 +0200

    util for measauring time

---
+++ utils/decorators.py
@@ -0,0 +1,11 @@
+import time
+
+
+def measure_time(function):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = function(*args, **kwargs)
+        end = time.time()
+        print(function.__name__, 'took ', end - start, 'seconds')
+        return ret
+    return wrapper
