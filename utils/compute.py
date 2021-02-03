f62117fdb8a8d4514ebcccceb2329ad59e153622
Glickman <glickman1@mail.tau.ac.il>
2021-02-03 14:01:44 +0200

    util to get gpu

---
+++ utils/compute.py
@@ -0,0 +1,10 @@
+import os
+
+
+def get_index_of_free_gpus(minimum_free_giga=4):
+    def get_free_gpu():
+        os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp_mem_allo')
+        memory_available = [int(x.split()[2]) for x in open('tmp_mem_allo', 'r').readlines()]
+        return {index: mb for index, mb in enumerate(memory_available)}
+
+    return [index for index,mega in get_free_gpu().items() if mega > minimum_free_giga*1000]
