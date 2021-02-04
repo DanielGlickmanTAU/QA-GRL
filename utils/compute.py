46773cb3971e7aa2659f146f51da1e5cc0c06b77
Glickman <glickman1@mail.tau.ac.il>
2021-02-04 08:46:25 +0200

    write gpu status to file

--- utils/compute.py
+++ utils/compute.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 
 
@@ -7,8 +8,15 @@ def get_index_of_free_gpus(minimum_free_giga=4):
         lines = os.popen('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free').readlines()
         memory_available = [int(x.split()[2]) for x in lines]
         return {index: mb for index, mb in enumerate(memory_available)}
+    def write_gpus_to_file(dict):
+        t = time.time()
+        with open(str(t)+'_gpu', 'w+') as f:
+            f.write(str(dict))
 
-    return [index for index, mega in get_free_gpu().items() if mega > minimum_free_giga * 1000]
+
+    gpus = get_free_gpu()
+    write_gpus_to_file(gpus)
+    return [index for index, mega in gpus.items() if mega > minimum_free_giga * 1000]
 
 
 def get_device():
