75d6bb3d65183c86b578b883178ddf8b9ca98900
Glickman <glickman1@mail.tau.ac.il>
2021-02-04 08:29:52 +0200

    get gpus directly from os command result and avoid writing to file

--- utils/compute.py
+++ utils/compute.py
@@ -2,10 +2,12 @@ import os
 import torch
 
 
+can do this first thing::::os.environ["CUDA_VISIBLE_DEVICES"]="4"
+
 def get_index_of_free_gpus(minimum_free_giga=4):
     def get_free_gpu():
-        os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp_mem_allo')
-        memory_available = [int(x.split()[2]) for x in open('tmp_mem_allo', 'r').readlines()]
+        lines = os.popen('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free').readlines()
+        memory_available = [int(x.split()[2]) for x in lines]
         return {index: mb for index, mb in enumerate(memory_available)}
 
     return [index for index, mega in get_free_gpu().items() if mega > minimum_free_giga * 1000]
