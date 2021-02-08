01074a8ff14e9b323c0e19e0f9662d4a3afc3fa2
Glickman <glickman1@mail.tau.ac.il>
2021-02-08 17:08:31 +0200

    minor

--- experiments/race.py
+++ experiments/race.py
@@ -4,10 +4,10 @@ from experiments.TaskParams import TaskParams
 from utils.datasets_loading import get_race_dataset
 from utils.model_loading import get_model_and_tokenizer_for_classification
 
+
 model, tokenizer = get_model_and_tokenizer_for_classification()
 
 encoded_dataset = get_race_dataset(tokenizer)
-
 
 
 @dataclass
