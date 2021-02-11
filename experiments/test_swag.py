dce85811f9ecfaa3b743dd8edcf73f41ce4aee85
Glickman <glickman1@mail.tau.ac.il>
2021-02-11 10:54:44 +0200

    change batch size

--- experiments/test_swag.py
+++ experiments/test_swag.py
@@ -10,7 +10,7 @@ import swag
 class Test(TestCase):
     def test_swag_classification_params(self):
         params = swag.swagClassificationParams
-        batch_size = 64
+        batch_size = 18
         metric_name = "accuracy"
         metric = load_metric(metric_name)
 
@@ -27,17 +27,18 @@ class Test(TestCase):
             learning_rate=2e-5,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
-            num_train_epochs=20,
+            num_train_epochs=50,
             weight_decay=0.01,
             # load_best_model_at_end=True,
             metric_for_best_model=metric_name,
+            overwrite_output_dir=True
         )
 
         trainer = Trainer(
             params.model,
             args,
-            train_dataset=params.dataset["train"].select(range(1_000)),
-            eval_dataset=params.dataset["train"].select(range(1_000)),
+            train_dataset=params.dataset["train"],
+            eval_dataset=params.dataset["validation"],
             tokenizer=params.tokenizer,
             compute_metrics=compute_metrics
         )
@@ -45,6 +46,6 @@ class Test(TestCase):
         print(torch.cuda.device_count())
         import os
         print(os.environ["CUDA_VISIBLE_DEVICES"])
-        results = trainer.train()
+        results = trainer.train(resume_from_checkpoint=params.benchmark_folder_name)
         print(results)
 
