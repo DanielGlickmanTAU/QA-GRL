5e790fca52958aac25b3ad3fffc3216bfc9a3126
Glickman <glickman1@mail.tau.ac.il>
2021-02-11 10:54:31 +0200

    delete previous model checkpoints to save memory. use more gpus

--- experiments/test_race.py
+++ experiments/test_race.py
@@ -32,7 +32,7 @@ print('yoyo')
 class Test(TestCase):
     def test_race_classification_params(self):
         params = race.classificationParams
-        batch_size = 2
+        batch_size = 16
         metric_name = "accuracy"
         metric = load_metric(metric_name)
 
@@ -60,21 +60,24 @@ class Test(TestCase):
             learning_rate=2e-5,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
-            num_train_epochs=30,
+            num_train_epochs=100,
             weight_decay=0.01,
-            load_best_model_at_end=True,
+            # load_best_model_at_end=True,
             metric_for_best_model=metric_name,
+            # overwrite_output_dir=True
+            save_total_limit=2
         )
 
         trainer = Trainer(
             params.model,
             args,
-            train_dataset=params.dataset["train"].select(range(100)),
-            eval_dataset=params.dataset["train"].select(range(100)),
+            train_dataset=params.dataset["train"],
+            eval_dataset=params.dataset["validation"],
             tokenizer=params.tokenizer,
             compute_metrics=compute_metrics
         )
 
+        #resume_from_checkpoint=params.benchmark_folder_name
         results = trainer.train()
         print('done train')
         print(results)
