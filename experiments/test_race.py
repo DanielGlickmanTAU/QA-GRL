9b020b6bf8bb6045c588d5be957951cbf5e1794c
Glickman <glickman1@mail.tau.ac.il>
2021-02-14 12:17:26 +0200

    clean up some junk

--- experiments/test_race.py
+++ experiments/test_race.py
@@ -1,32 +1,12 @@
-print('0')
 from unittest import TestCase
 
 from utils import compute
-from utils.compute import get_index_of_free_gpus
 
 torch = compute.get_torch()
 from datasets import load_metric
 from transformers import TrainingArguments, Trainer
 
-print('1')
 import race
-
-print('2')
-# import pytorch_lightning as pl
-from dataclasses import dataclass
-import torch.functional as F
-from experiments.TaskParams import TaskParams
-
-print('3')
-# from models.ClassificationModel import ClassificationModel
-
-print('3.5')
-from utils.datasets_loading import get_race_dataset
-
-print('4')
-from utils.model_loading import get_model_and_tokenizer_for_classification
-
-print('yoyo')
 
 
 class Test(TestCase):
@@ -40,18 +20,6 @@ class Test(TestCase):
             predictions, labels = eval_pred
             predictions = predictions.argmax(axis=1)
             return metric.compute(predictions=predictions, references=labels)
-
-        model, tokenizer = get_model_and_tokenizer_for_classification()
-        dataset = get_race_dataset(tokenizer)
-
-        def get_data_loader(split):
-            return torch.utils.data.DataLoader(dataset[split], batch_size=2, num_workers=2)
-
-        # qaClassificationModel = ClassificationModel(model, tokenizer)
-
-        # # trainer = pl.Trainer(gpus=len(get_index_of_free_gpus()))
-        # trainer = pl.Trainer(gpus=len(get_index_of_free_gpus()),distributed_backend='dp')
-        # trainer.fit(qaClassificationModel, get_data_loader('train'))
 
         args = TrainingArguments(
 
@@ -77,7 +45,7 @@ class Test(TestCase):
             compute_metrics=compute_metrics
         )
 
-        #resume_from_checkpoint=params.benchmark_folder_name
+        # resume_from_checkpoint=params.benchmark_folder_name
         results = trainer.train(params.benchmark_folder_name + '/checkpoint-105500')
         print('done train')
         print(results)
