6c5ecde1401b4073a22f7e66a4395dab57e37be8
Glickman <glickman1@mail.tau.ac.il>
2021-02-14 12:03:43 +0200

    minor

--- utils/compute.py
+++ utils/compute.py
@@ -1,8 +1,8 @@
 import os
 import time
 
-minimum_free_giga = 10
-max_num_gpus = 4
+minimum_free_giga = 8
+max_num_gpus = 5
 
 last_write = 0
 home = '/specific/netapp5_3/ML_courses/students/DL2020/glickman1'
