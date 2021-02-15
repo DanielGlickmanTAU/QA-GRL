139cbff1726426f6fc070b06b02b61c100f308ae
Glickman <glickman1@mail.tau.ac.il>
2021-02-15 18:41:00 +0200

    log hyperparams

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,4 +1,4 @@
-def start_experiment(tags=[]):
+def start_experiment(tags=[], hyperparams = {}):
     from comet_ml import Experiment
     experiment = Experiment('FvAd5fm5rJLIj6TtmfGHUJm4b', project_name='dl', workspace="danielglickmantau")
 
@@ -7,5 +7,7 @@ def start_experiment(tags=[]):
 
     if len(tags):
         experiment.add_tags(tags)
+    if len(hyperparams):
+        experiment.log_parameters(hyperparams)
 
     return torch, experiment
