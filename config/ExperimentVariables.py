995dfbba99bff28002be7f14fe422208562d525f
Glickman <glickman1@mail.tau.ac.il>
2021-02-16 15:11:24 +0200

    old task

--- config/ExperimentVariables.py
+++ config/ExperimentVariables.py
@@ -30,7 +30,7 @@ hyperparams = AttrDict()
 hyperparams.use_unique_seperator_for_answer = True
 hyperparams.return_overflowing_tokens = False
 hyperparams.disable_tqdm = True
-hyperparams.task_name = 'sst'
+hyperparams.task_name = 'race'
 race = _race()
 
 hyperparams.race = race
