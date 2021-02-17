b28698caedd9d76c177dce909d457e5f69a6bfa9
Glickman <glickman1@mail.tau.ac.il>
2021-02-17 11:46:54 +0200

    minor

--- experiments/test_load_best_model.py
+++ experiments/test_load_best_model.py
@@ -1,9 +1,8 @@
-from config.ExperimentVariables import hyperparams
 from unittest import TestCase
-import tasks
+
 from utils import compute
-from utils.model_loading import get_model_and_tokenizer_for_classification
 from utils import special_tokens
+from utils.model_loading import get_model_and_tokenizer_for_classification
 
 torch = compute.get_torch()
 
@@ -15,13 +14,13 @@ class Test(TestCase):
         model_pretrained, tokenizer_pre = get_model_and_tokenizer_for_classification(model_name=model_name)
         model_fine_tuned, tokenizer_fine = get_model_and_tokenizer_for_classification(model_name=cached_dir)
 
-        special_token_tensor = self.tokenize(tokenizer_fine,special_tokens.OPT)
+        special_token_tensor = self.tokenize(tokenizer_fine, special_tokens.OPT)
         assert len(special_token_tensor) == 1
 
         initial_embeddings = self.get_embedding_of_OPT_by_model(model_pretrained, special_token_tensor)
         learned_embedding = self.get_embedding_of_OPT_by_model(model_fine_tuned, special_token_tensor)
 
-        #assert the embedding changed during learning
+        # assert the embedding changed during learning
         assert (learned_embedding - initial_embeddings).norm() > 0.01
 
         existing_token_tensor = self.tokenize(tokenizer_fine, 'the')
@@ -29,8 +28,9 @@ class Test(TestCase):
         existing_initial_embeddings = self.get_embedding_of_OPT_by_model(model_pretrained, existing_token_tensor)
         existing_learned_embedding = self.get_embedding_of_OPT_by_model(model_fine_tuned, existing_token_tensor)
 
-        #see that the change in embedding for a new word is indeed more significant than for a word already in vocab.
-        assert (learned_embedding - initial_embeddings).norm() > 5 * (existing_initial_embeddings - existing_learned_embedding).norm()
+        # see that the change in embedding for a new word is indeed more significant than for a word already in vocab.
+        assert (learned_embedding - initial_embeddings).norm() > 5 * (
+                    existing_initial_embeddings - existing_learned_embedding).norm()
 
     def tokenize(self, tokenizer_fine, string):
         speical_token_encoded = tokenizer_fine.encode(string)[1:-1]
@@ -39,4 +39,3 @@ class Test(TestCase):
 
     def get_embedding_of_OPT_by_model(self, model, special_token_tensor):
         return model.base_model.embeddings.word_embeddings(special_token_tensor.to(compute.get_device()))
-
