6a8ea0727e8f807c2f74fe4547d446b3abf8069a
Glickman <glickman1@mail.tau.ac.il>
2021-02-17 10:36:20 +0200

    refactor

--- experiments/tasks.py
+++ experiments/tasks.py
@@ -20,20 +20,20 @@ class SSTClassificationParams(TaskParams):
     benchmark_folder_name: str = "sst-classification"
 
 
-model, tokenizer = get_model_and_tokenizer_for_classification()
-
-
 def get_race_classification_params():
+    model, tokenizer = get_model_and_tokenizer_for_classification()
     encoded_dataset = get_race_dataset(tokenizer)
     return RACEClassificationParams(encoded_dataset, model, tokenizer)
 
 
 def get_sst_params():
+    model, tokenizer = get_model_and_tokenizer_for_classification()
     encoded_dataset = get_sst_dataset(tokenizer)
     return SSTClassificationParams(encoded_dataset, model, tokenizer)
 
 
 def get_swag_params():
+    model, tokenizer = get_model_and_tokenizer_for_classification()
     encoded_dataset = get_swag_dataset(tokenizer)
     return SwagClassificationParams(encoded_dataset, model, tokenizer)
 
