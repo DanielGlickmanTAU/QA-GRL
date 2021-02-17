52e52c9a94138305cfd19921bdcc18c6dbab5a4f
Glickman <glickman1@mail.tau.ac.il>
2021-02-17 11:44:06 +0200

    test for encoding

---
+++ experiments/test_load_best_model.py
@@ -0,0 +1,42 @@
+from config.ExperimentVariables import hyperparams
+from unittest import TestCase
+import tasks
+from utils import compute
+from utils.model_loading import get_model_and_tokenizer_for_classification
+from utils import special_tokens
+
+torch = compute.get_torch()
+
+
+class Test(TestCase):
+    def test_opt_encoding_is_learned(self):
+        model_name = 'deepset/roberta-base-squad2'
+        cached_dir = "race-classification" + '/' + model_name + '/checkpoint-53500'
+        model_pretrained, tokenizer_pre = get_model_and_tokenizer_for_classification(model_name=model_name)
+        model_fine_tuned, tokenizer_fine = get_model_and_tokenizer_for_classification(model_name=cached_dir)
+
+        special_token_tensor = self.tokenize(tokenizer_fine,special_tokens.OPT)
+        assert len(special_token_tensor) == 1
+
+        initial_embeddings = self.get_embedding_of_OPT_by_model(model_pretrained, special_token_tensor)
+        learned_embedding = self.get_embedding_of_OPT_by_model(model_fine_tuned, special_token_tensor)
+
+        #assert the embedding changed during learning
+        assert (learned_embedding - initial_embeddings).norm() > 0.01
+
+        existing_token_tensor = self.tokenize(tokenizer_fine, 'the')
+
+        existing_initial_embeddings = self.get_embedding_of_OPT_by_model(model_pretrained, existing_token_tensor)
+        existing_learned_embedding = self.get_embedding_of_OPT_by_model(model_fine_tuned, existing_token_tensor)
+
+        #see that the change in embedding for a new word is indeed more significant than for a word already in vocab.
+        assert (learned_embedding - initial_embeddings).norm() > 5 * (existing_initial_embeddings - existing_learned_embedding).norm()
+
+    def tokenize(self, tokenizer_fine, string):
+        speical_token_encoded = tokenizer_fine.encode(string)[1:-1]
+        special_token_tensor = torch.tensor(speical_token_encoded)
+        return special_token_tensor
+
+    def get_embedding_of_OPT_by_model(self, model, special_token_tensor):
+        return model.base_model.embeddings.word_embeddings(special_token_tensor.to(compute.get_device()))
+
