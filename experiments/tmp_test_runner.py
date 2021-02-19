cfa365a4fba4dff6a33c1a03aece306d1a2f5134
Glickman <glickman1@mail.tau.ac.il>
2021-02-19 10:41:23 +0200

    comment

--- experiments/tmp_test_runner.py
+++ experiments/tmp_test_runner.py
@@ -1,5 +1,6 @@
 from experiments.test_race import Test
 
+#used because pychamrm is problematic with running the same tests twice in the same time.
 if __name__ == '__main__':
     print('starting test')
     Test().test_race_classification_params()
