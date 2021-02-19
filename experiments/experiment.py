a08ca431e94dc32d952521b0fd55e988abd2d3e3
Glickman <glickman1@mail.tau.ac.il>
2021-02-19 17:47:52 +0200

    format imports

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,10 +1,14 @@
 import collections
-def start_experiment(tags=[], hyperparams = {}):
+from comet_ml import Experiment
+from utils import compute
+
+
+def start_experiment(tags=[], hyperparams={}):
     def flatten(d):
         items = []
         for k, v in d.items():
             if isinstance(v, collections.MutableMapping):
-                if hasattr(v,'__dict__'):
+                if hasattr(v, '__dict__'):
                     items.extend(flatten(v.__dict__).items())
                 else:
                     items.extend(flatten(v).items())
@@ -12,10 +16,8 @@ def start_experiment(tags=[], hyperparams = {}):
                 items.append((k, v))
         return dict(items)
 
-    from comet_ml import Experiment
     experiment = Experiment('FvAd5fm5rJLIj6TtmfGHUJm4b', project_name='dl', workspace="danielglickmantau")
 
-    from utils import compute
     torch = compute.get_torch()
 
     if len(tags):
