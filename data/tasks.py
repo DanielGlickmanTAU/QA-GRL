a5fd518500b946303b26549bc2585ab73e7a3f0f
Glickman <glickman1@mail.tau.ac.il>
2021-02-20 11:50:57 +0200

    add combined dataset

--- data/tasks.py
+++ data/tasks.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from data.TaskParams import TaskParams
 from data.datasets_loading import get_race_dataset, get_sst_dataset, get_swag_dataset
 from utils.model_loading import get_model_and_tokenizer_for_classification
+import datasets
 
 
 @dataclass
@@ -18,6 +19,11 @@ class RACEClassificationParams(TaskParams):
 @dataclass
 class SSTClassificationParams(TaskParams):
     benchmark_folder_name: str = "sst-classification"
+
+
+@dataclass
+class CombinedClassificationParams(TaskParams):
+    benchmark_folder_name: str = "combined-classification"
 
 
 def get_race_classification_params():
@@ -38,8 +44,16 @@ def get_swag_params():
     return SwagClassificationParams(encoded_dataset, model, tokenizer)
 
 
+def get_combined_params():
+    model, tokenizer = get_model_and_tokenizer_for_classification()
+    encoded_dataset = datasets.concatenate_datasets(
+        [get_swag_dataset(tokenizer), get_race_dataset(tokenizer), get_sst_dataset(tokenizer)])
+    return CombinedClassificationParams(encoded_dataset, model, tokenizer)
+
+
 task_to_params_getter = {
     'swag': get_swag_params,
     'race': get_race_classification_params,
-    'sst': get_sst_params
+    'sst': get_sst_params,
+    'combined': get_combined_params
 }
