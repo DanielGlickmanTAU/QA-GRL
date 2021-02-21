9be20e48542c783b71a80b7294d8d07b01325977
Glickman <glickman1@mail.tau.ac.il>
2021-02-21 17:10:57 +0200

    handle mutable defaults

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -3,7 +3,12 @@ from comet_ml import Experiment
 from utils import compute
 
 
-def start_experiment(tags=[], hyperparams={}):
+def start_experiment(tags=None, hyperparams=None):
+    if hyperparams is None:
+        hyperparams = {}
+    if tags is None:
+        tags = []
+
     def flatten(d):
         items = []
         for k, v in d.items():
@@ -17,7 +22,6 @@ def start_experiment(tags=[], hyperparams={}):
         return dict(items)
 
     experiment = Experiment('FvAd5fm5rJLIj6TtmfGHUJm4b', project_name='dl', workspace="danielglickmantau")
-
     torch = compute.get_torch()
 
     if len(tags):
