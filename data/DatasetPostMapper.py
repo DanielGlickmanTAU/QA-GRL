bd7100b685f91418f0031cc4432ea6dacde31653
Glickman <glickman1@mail.tau.ac.il>
2021-02-27 12:27:35 +0200

    datasets mapping

---
+++ data/DatasetPostMapper.py
@@ -0,0 +1,29 @@
+from utils import compute
+
+torch = compute.get_torch()
+from data.TaskParams import TaskParams
+
+
+class DataSetPostMapper:
+    def __init__(self, task_params: TaskParams):
+        self.dataset = task_params.dataset
+        self.model = task_params.model
+
+        self.tokenizer = task_params.tokenizer
+
+    def _tensor(self, lst):
+        return torch.tensor(lst, device=compute.get_device())
+    def _numpy(self,tensor):
+        return tensor.detach().cpu().numpy()
+
+    def add_is_correct_and_probs(self, examples):
+        self.model.eval()
+
+        logits = self.model(self._tensor(examples['input_ids']),
+                                               self._tensor(examples['attention_mask'])).logits
+        probs = logits.softmax(dim=1).max(dim=1).values
+        predictions = logits.argmax(dim=1)
+        correct = [1 if y_hat == y else 0 for y, y_hat in
+                   zip(examples['label'], predictions)]
+
+        return {'probs': self._numpy(probs), 'predictions': self._numpy(predictions), 'correct': correct}
--- experiments/tests/test_diff_between_models.py
+++ experiments/tests/test_diff_between_models.py
@@ -1,5 +1,7 @@
 from utils import compute
+
 torch = compute.get_torch()
+from data.DatasetPostMapper import DataSetPostMapper
 from transformers import TrainingArguments, Trainer
 from config import ExperimentVariables
 from config.ExperimentVariables import hyperparams
@@ -19,19 +21,40 @@ import os
 
 class Test(TestCase):
     def test_diff_between_models(self):
-        task = 'race-classification'
+        task = 'boolq-classification'
         model_params = ExperimentVariables._distilbert_squad
         model_params.num_epochs = 0
         model, tokenizer = get_last_model_and_tokenizer(task, model_params)
-        ds = datasets_loading.get_race_dataset(tokenizer)
+
+        ds = datasets_loading.get_boolq_dataset(tokenizer)
 
         task_params = TaskParams(ds, model, tokenizer, 'trash')
+
+        # add DataSetPostMapper(model_params,task_params)
+        # will create trainer
+        # add column was_model_right, model_prob
+        mapper = DataSetPostMapper(task_params)
+        mapped_ds = ds.map(mapper.add_is_correct_and_probs , batched=True)
+
+        print(mapped_ds[:10])
+
         trainer = training.get_trainer('trash', model_params, task_params, False)
-                                       #, experiment=experiment)
+        # , experiment=experiment)
         m1_out_train = trainer.predict(ds['train'])
         m1_out_valid = trainer.predict(ds['validation'])
-        m1_out_train = trainer.predict(ds['train'])
-        print(m1_out_valid)
+        probs = torch.tensor(m1_out_train.predictions[:10]).softmax(dim=1)
+        predictions = torch.tensor(m1_out_train.predictions[:10]).argmax(dim=1)
+        correct = [1 if y_hat == y else 0 for y, y_hat in
+                   zip(m1_out_train.label_ids[:10000], torch.tensor(m1_out_train.predictions[:10000]).argmax(dim=1))]
+
+        predict_manually_logits: torch.Tensor = \
+        trainer.prediction_step(model, {'input_ids': torch.tensor(ds['validation']['input_ids'][:10]),
+                                        'attention_mask': torch.tensor(ds['validation']['attention_mask'][:10])},
+                                False)[1]
+        same_thing_logits = model.forward(torch.tensor(ds['train']['input_ids'][:10]).to(compute.get_device()),
+                                          torch.tensor(ds['train']['attention_mask'][:10]).to(
+                                              compute.get_device())).logits
+
         """PredictionOutput(predictions=array([[-0.7137633 ,  0.6313474 ],
        [-0.05348547,  0.16040793],
        [-0.79939294,  0.72003627],
