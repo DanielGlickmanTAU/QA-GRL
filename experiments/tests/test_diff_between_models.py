7c1a6102d1548ba5264df272891e62d8b627f752
Glickman <glickman1@mail.tau.ac.il>
2021-03-01 10:09:18 +0200

    load latest confidence model from memory

--- experiments/tests/test_diff_between_models.py
+++ experiments/tests/test_diff_between_models.py
@@ -88,20 +88,22 @@ class Test(TestCase):
         return mapped_ds
 
 
-def get_confidence_model(mapped_qa_ds, model_params):
-    save_dir = get_save_path('error-prediction', model_params)
-    confidence_model, confidence_tokenizer = model_loading.get_model_and_tokenizer_for_classification(
-        model_params.model_name, model_params.model_tokenizer)
-    train_confidence_model(confidence_model, confidence_tokenizer, mapped_qa_ds, model_params, save_dir)
+def get_confidence_model(mapped_qa_ds, model_params, train=False):
+    task_name = 'error-prediction'
+    if train:
+        confidence_model, confidence_tokenizer = model_loading.get_model_and_tokenizer_for_classification(
+            model_params.model_name, model_params.model_tokenizer)
+        train_confidence_model(confidence_model, confidence_tokenizer, mapped_qa_ds, model_params, task_name)
 
-    return confidence_model, confidence_tokenizer
+    return get_last_model_and_tokenizer(task_name, model_params)
 
 
-def train_confidence_model(confidence_model, confidence_tokenizer, mapped_qa_ds, model_params, save_dir):
+def train_confidence_model(confidence_model, confidence_tokenizer, mapped_qa_ds, model_params, task_name):
     mapper = DataSetPostMapper(confidence_model, confidence_tokenizer)
     error_ds = mapped_qa_ds.map(mapper.change_labels)
     metric_name = "accuracy"
     task_params = TaskParams(error_ds, confidence_model, confidence_tokenizer, 'error-prediction')
+    save_dir = get_save_path(task_name, model_params)
     trainer = get_trainer(save_dir, model_params, task_params, True, experiment, metric_name,
                           hyperparams.disable_tqdm)
     trainer.train()
