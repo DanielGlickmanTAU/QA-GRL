89706e461efa18b59f5582938079eecad61346ed
Glickman <glickman1@mail.tau.ac.il>
2021-03-01 17:09:51 +0200

    wip

--- experiments/tests/question_generatoin_test.py
+++ experiments/tests/question_generatoin_test.py
@@ -1,4 +1,5 @@
 from utils import compute
+from data import question_generation_dataset
 
 torch = compute.get_torch()
 from config import ExperimentVariables
@@ -16,6 +17,7 @@ class Test(TestCase):
         model_params = ExperimentVariables._t5_qg
 
         model_type = 't5'
-        tokenizer = get_model_and_tokenizer_for_qa_generation(model_params)
+        model, tokenizer = get_model_and_tokenizer_for_qa_generation(model_params)
+        boolq = question_generation_dataset.get_processed_boolq_dataset(tokenizer)
 
         print(tokenizer)
