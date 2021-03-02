8624744740a48d7ed2497e01efc4f4f8b64d0257
Glickman <glickman1@mail.tau.ac.il>
2021-03-02 15:02:36 +0200

    extract class

---
+++ models/question_generation.py
@@ -0,0 +1,72 @@
+from transformers import PreTrainedModel, PreTrainedTokenizer
+
+from utils import compute
+
+
+class E2EQGPipeline:
+    def __init__(
+            self,
+            model: PreTrainedModel,
+            tokenizer: PreTrainedTokenizer,
+    ):
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+        self.device = compute.get_device()
+        self.model.to(self.device)
+
+        self.default_generate_kwargs = {
+            "max_length": 256,
+            "num_beams": 4,
+            "length_penalty": 1.5,
+            "no_repeat_ngram_size": 3,
+            "early_stopping": True,
+        }
+
+    def __call__(self, context: str, **generate_kwargs):
+        inputs = self._prepare_inputs_for_e2e_qg(context)
+
+        if not generate_kwargs:
+            generate_kwargs = self.default_generate_kwargs
+
+        input_length = inputs["input_ids"].shape[-1]
+
+        outs = self.model.generate(
+            input_ids=inputs['input_ids'].to(self.device),
+            attention_mask=inputs['attention_mask'].to(self.device),
+            **generate_kwargs
+        )
+
+        prediction = self.tokenizer.decode(outs[0], skip_special_tokens=True)
+        questions = prediction.split("<sep>")
+        questions = [question.strip() for question in questions[:-1]]
+        return questions
+
+    def _prepare_inputs_for_e2e_qg(self, context):
+        source_text = context
+        if not context.startswith("generate questions:"):
+            source_text = f"generate questions: {context}"
+        source_text = source_text + " </s>"
+
+        inputs = self._tokenize([source_text], padding=False)
+        return inputs
+
+    def _tokenize(
+            self,
+            inputs,
+            padding=True,
+            truncation=True,
+            add_special_tokens=True,
+            max_length=512
+    ):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs,
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt"
+        )
+        return inputs
