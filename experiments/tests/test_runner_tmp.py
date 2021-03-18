bdea1e981f546e0212e0f45fbf6a8e23cb9939fd
Glickman <glickman1@mail.tau.ac.il>
2021-03-18 19:27:13 +0200

    adjust code for training specific classifier and rename some stuff

--- experiments/tests/test_runner_tmp.py
+++ experiments/tests/test_runner_tmp.py
@@ -3,4 +3,4 @@ from experiments.test_race import Test
 #used because pychamrm is problematic with running the same tests twice in the same time.
 if __name__ == '__main__':
     print('starting test')
-    Test().test_race_classification_params()
+    Test().train_boolq_test()
--- experiments/tests/test_train.py
+++ experiments/tests/test_train.py
@@ -10,23 +10,23 @@ from data import tasks
 
 
 class Test(TestCase):
-    def test_race_classification_params(self):
+    def train_boolq_test(self):
         model_params = variables._distilbert_squad.clone()
+        task_name = 'boolq@1'
+        model_params.num_epochs = 1
+
+        self.run_exp(model_params, task_name)
+
+    def run_exp(self, model_params, task_name):
         model_name = model_params.model_name
         torch, exp = experiment.start_experiment(tags=[hyperparams.env, model_name, hyperparams.task_name],
                                                  hyperparams=hyperparams)
-
-        task_name = 'boolq@1'
         params = tasks.get_task_params(task_name, model_params.model_name, model_params.model_tokenizer)
         save_dir = get_save_path(params.benchmark_folder_name, model_params)
         print('saving to ', save_dir)
-
         metric_name = "accuracy"
-
-        model_params.num_epochs = 1
         trainer = get_trainer(save_dir, hyperparams.model_params, params, True, exp, metric_name,
                               hyperparams.disable_tqdm)
-
         # results = trainer.train(save_dir + '/checkpoint-84500')
         # results = trainer.train(save_dir + '/checkpoint-8474')
         results = trainer.train()
@@ -36,4 +36,4 @@ class Test(TestCase):
 
 if __name__ == '__main__':
     print('starting test')
-    Test().test_race_classification_params()
+    Test().train_boolq_test()
