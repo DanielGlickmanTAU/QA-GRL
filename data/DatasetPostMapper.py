45ac75436fdf0443763e826aa1f3f20952095749
Glickman <glickman1@mail.tau.ac.il>
2021-03-20 14:52:56 +0200

    adding prob given by the model for the correct answer

--- data/DatasetPostMapper.py
+++ data/DatasetPostMapper.py
@@ -29,6 +29,20 @@ class DataSetPostMapper:
 
     def add_prob_to_be_correct(self, examples, label_name='prob_correct'):
         """ runs model on examples and adds the prob to be correct. as apposed to add_is_correct_and_probs, which add max prob by the model, even if it is false answer """
+        self.model.eval()
+        with torch.no_grad():
+            logits = self.model(self._tensor(examples['input_ids']),
+                                self._tensor(examples['attention_mask'])).logits
+            probs = logits.softmax(dim=1).max(dim=1).values
+            predictions = logits.argmax(dim=1)
+            correct = [1 if y_hat == y else 0 for y, y_hat in
+                       zip(examples['label'], predictions)]
+
+            prob_correct = []
+            for p, c in zip(probs, correct):
+                prob_correct.append(p if c else (1 - p))
+
+            return {label_name: prob_correct}
 
     def add_probs(self, examples):
         self.model.eval()
--- experiments/tests/test_DatasetPostMapper.py
+++ experiments/tests/test_DatasetPostMapper.py
@@ -7,23 +7,25 @@ from utils import compute
 class TestDataSetPostMapper(TestCase):
     def test_add_prob_to_be_correct(self):
         torch = compute.get_torch()
-        logits = [[10, 0], [10, 0], [1, 1]]
+        #[10,0] logits means model thinks it is false
+        logits = [[10., 0.], [10., 0], [1., 1.]]
         labels = [1, 0, 1]
 
-        class MockModel():
+        class MockModel:
             def eval(self):
                 pass
 
             def __call__(self, *args, **kwargs):
                 class _logits():
-                    self.logits = torch.tensor(logits)
+                    def __init__(self):
+                        self.logits = torch.tensor(logits)
 
                 return _logits()
 
-        mapper = DataSetPostMapper(MockModel, None)
+        mapper = DataSetPostMapper(MockModel(), None)
         examples = {
-            'input_ids': None,
-            'attention_mask': None,
+            'input_ids': torch.tensor(1.),
+            'attention_mask': torch.tensor(1.),
             'label': labels
         }
 
