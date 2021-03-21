b7efd1becb92505540b8eafee6e676582b7ed3de
Glickman <glickman1@mail.tau.ac.il>
2021-03-21 19:59:32 +0200

    minor

--- experiments/scoring_functions.py
+++ experiments/scoring_functions.py
@@ -3,7 +3,7 @@ import numpy
 
 # smart minus stupid
 def smart_is_right_and_stupid_is_wrong(smart_scores, stupid_scores):
-    normalizer = len(stupid_scores) / len(smart_scores)
+    normalizer = ratio(smart_scores, stupid_scores)
     return (normalizer * sum(smart_scores)) - sum(stupid_scores)
 
 
@@ -21,3 +21,8 @@ def var_combined(smart_scores, stupid_scores):
 
 def var_stupid_minus_smart(smart_scores, stupid_scores):
     return numpy.var(stupid_scores) - numpy.var(smart_scores)
+
+
+def ratio(smart_scores, stupid_scores):
+    assert len(stupid_scores) >= len(smart_scores)
+    return len(stupid_scores) / len(smart_scores)
