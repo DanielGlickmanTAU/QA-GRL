b5a1dcc29d7bb991aefe47c535f8fef8e712c0b3
Glickman <glickman1@mail.tau.ac.il>
2021-03-21 17:37:21 +0200

    add scoring functions

--- experiments/scoring_functions.py
+++ experiments/scoring_functions.py
@@ -1,6 +1,22 @@
+import numpy
+
+
 def smart_minus_stupid(smart_scores, stupid_scores):
     normalizer = len(stupid_scores) / len(smart_scores)
     return (normalizer * sum(smart_scores)) - sum(stupid_scores)
 
-def absolute_error(smart_scores,stupid_scores):
+
+def absolute_error(smart_scores, stupid_scores):
     return sum(smart_scores) + sum(stupid_scores)
+
+
+def var_combined(smart_scores, stupid_scores):
+    return numpy.var(smart_scores + stupid_scores)
+
+
+def var_stupid_minus_smart(smart_scores, stupid_scores):
+    return numpy.var(stupid_scores) - numpy.var(smart_scores)
+
+
+def smart_is_right_and_stupid_are_not_sure(smart_scores, stupid_scores):
+    sum(smart_scores) - sum([numpy.abs(0.5 - x) for x in stupid_scores])
